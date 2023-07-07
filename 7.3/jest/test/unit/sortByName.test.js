5ac74bd441d85b22b49f3195072ece828639034e
mezitest <mezis4u@inbox.ru>
2023-07-07 12:37:29 +0600

    Complete the HW3 Task1

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,55 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
+  });
+
+  test("Books names should not be sorted if identical name", () => {
+    const input = [
+      "Властелин Колец",
+      "Властелин Колец",
+      "Властелин Колец",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Властелин Колец",
+      "Властелин Колец",
+    ];
+
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
+  });
+
+  test("Books names should not be sorted if one book", () => {
+    const input = [
+      "",
+      "Властелин Колец"
+    ];
+
+    const expected = [
+      "",
+      "Властелин Колец"
+    ];
+
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
+  });
+
+  test("Sorting throws exception without params", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError);
   });
 });
