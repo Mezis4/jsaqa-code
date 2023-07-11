a5108766883babc925f5dde451e4246153081eab
mezitest <mezis4u@inbox.ru>
2023-07-11 13:30:13 +0600

    Complete HomeWork 4

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,18 +9,23 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  }, 30000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  }, 10000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 8000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -29,6 +33,29 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    expect(actual).toContain("Get started with Team")
+  }, 8000);
+});
+
+describe("Github other pages tests", () => {
+  test("Feature page header content", async () => {
+    await page.goto("https://github.com/features");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Features | GitHub · GitHub');
+  }, 10000);
+
+  test("Security page header content", async () => {
+    await page.goto("https://github.com/security");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub Security · GitHub');
+  }, 10000);
+
+  test("Enterprise page header content", async () => {
+    await page.goto("https://github.com/enterprise");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Enterprise · A smarter way to work together · GitHub');
+  }, 10000);
 });
